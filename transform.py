18f1cc860f84cab7559b36c6e310f7f7bb9c6823
TrebleBlue <100921695+WampB@users.noreply.github.com>
2023-12-24 09:30:31 +0800

    Update transform.py
    现在可以直接在命令行运行该脚本从而将同一目录下名为m4a的文件夹中指定的m4a文件转换为mp3格式并将其存储到同一目录下的mp3文件夹中。可以一次转换多个文件，只要将它们用空格分开即可（这也意味着歌曲名称中不能有空格）。也可以选择命令行运行该脚本，不输入参数，稍后按照提示输入歌曲名称即可，此时歌曲名称可以包含空格。
    示例：
    cmd python.exe transform.py 晴天 The_Sound_of_Silence

--- transform.py
+++ transform.py
@@ -1,8 +1,28 @@
+from math import e
 from pydub import AudioSegment
+import sys,shutil
 
 def trans_m4a_to_other(filepath, hz):
     song = AudioSegment.from_file(filepath)
     song.export(filepath.split(".")[0].split("//")[-1]+ "." + str(hz), format=str(hz))
-
-path=""
-trans_m4a_to_other(path, "mp3")
+if __name__=='__main__':
+    try:
+        print(sys.argv[1:])
+        for i in sys.argv[1:]:
+            src='m4a\\'+i+'.m4a';target='mp3\\'+i+'.mp3'
+            trans_m4a_to_other(src,'mp3')
+            print(f'《{i}》 has been transformed from .m4a to .mp3.')
+            frm='m4a\\'+i+'.mp3'
+            shutil.move(src=frm,dst=target)
+            print(f'《{i}》 has been moved from m4a\ to mp3\.')
+        input('CMD transform has been completed, input to ensure exit.')
+    except:
+        name=input('Please input the song\'s name to be transformed.')
+        src='m4a\\'+name+'.m4a';target='mp3\\'+name+'.mp3'
+        trans_m4a_to_other(src,'mp3')
+        print(f'《{name}》 has been transformed from .m4a to .mp3.')
+        target='m4a\\'+name+'.mp3'
+        shutil.move(src=src,dst=target)
+        print(f'《{name}》 has been transformed from .m4a to .mp3.')
+        input('INPUT transform has been completed, input to ensure exit.')
+    pass
